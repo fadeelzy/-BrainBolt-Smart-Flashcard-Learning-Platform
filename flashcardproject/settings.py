1166b06abe3624954e69ea1b4b6d2e917ceacefc
Fadilah Abdulkadir <fadeelzy@gmail.com>
2025-10-20 14:52:40 +0100

    removed sensitive info

--- flashcardproject/settings.py
+++ flashcardproject/settings.py
@@ -73,16 +73,19 @@ WSGI_APPLICATION = 'flashcardproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'flashcard_db',
-    'USER': 'postgres',
-    'PASSWORD': 'Abdul12345!',
-    'HOST': 'localhost',
-    'PORT': '5432',
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
+    }
 }
+
+DEBUG = os.getenv('DEBUG') == 'True'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
